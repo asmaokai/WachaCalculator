01b83d663d3e28bbf85b5dbe53539ce35278d2a9
Jacek Dzienisiewicz <asmaokai1@gmail.com>
2024-02-25 17:47:35 +0100

    Initial Commit

---
+++ WachaCalculator/WachaCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WachaCalculatorApp.swift
+//  WachaCalculator
+//
+//  Created by Jacek Dzienisiewicz on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WachaCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
